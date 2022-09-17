fb683699e22b6dff5f407a054e00ef6c674ada63
charmed <cutqqq@mail.ru>
2022-09-18 00:40:33 +0500

    16

---
+++ dz3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+string input1 = Console.ReadLine();
+int number1 = int.Parse(input1);
+if (number1 % 2==1)
+{
+        Console.WriteLine("число нечетное");
+           
+}
+else
+{
+         Console.WriteLine("Число четное");
+}
