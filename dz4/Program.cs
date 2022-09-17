4363971ce1194a4f332d4f65e973e724ca5b8a71
charmed <cutqqq@mail.ru>
2022-09-18 01:19:39 +0500

    ком15

---
+++ dz4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+string input1 = Console.ReadLine();
+int n = int.Parse(input1);
+    
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
