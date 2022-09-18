cba6562e500ef2d748a4067a5ea11a78cf0e696c
charmed <cutqqq@mail.ru>
2022-09-18 20:32:40 +0500

    комит23

---
+++ example11/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = { 1, 2, 3, 4, 5, 17};
+int n = array.Length;
+int find =4;
+
+int index = 0;
+while (index<n)
+{
+if(array[index] == find)
+{
+    Console.WriteLine(index);
+    break;
+}
+//index = index +1;
+index++;
+}
