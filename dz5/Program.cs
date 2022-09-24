ef271f94bb2f2ad2759f41a0bf1e992debb0e393
charmed <cutqqq@mail.ru>
2022-09-24 14:23:08 +0500

    комент25

---
+++ dz5/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+int seconddigit = (number/10) % 10;
+Console.WriteLine(seconddigit);
