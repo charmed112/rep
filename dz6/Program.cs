7245734b75a85510b216db0b965f0968c4bde60c
charmed <cutqqq@mail.ru>
2022-09-25 20:40:49 +0500

    комит25

---
+++ dz6/Program.cs
@@ -0,0 +1,11 @@
+﻿  System.Console.WriteLine("введите число");
+  int number = Convert.ToInt32(Console.ReadLine());
+  string numbertext = Convert.ToString(number);
+  if (numbertext.Length > 2)
+  {
+    Console.WriteLine(numbertext[2]);
+  }
+  else 
+  {
+    Console.WriteLine("третьей цифры нет");
+  }
