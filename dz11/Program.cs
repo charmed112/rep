67f55688220c5653d98a93ae75dfb429914482d8
charmed <cutqqq@mail.ru>
2022-09-28 18:41:50 +0500

    комиты 27

---
+++ dz11/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+int count = 1;
+while (count <= number)
+{
+    Console.Write(Math.Pow(count, 3));
+    if (count != number)
+    Console.Write(",");
+    count++;
+    
+}
