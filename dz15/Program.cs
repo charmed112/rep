779a93bc24b81612746ffd1ff3cdc91f842d8799
charmed <cutqqq@mail.ru>
2022-10-03 20:41:34 +0500

    комит29

---
+++ dz15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+CreateArray(array);
+PrintArray(array);
+//создание рандомного массива
+void CreateArray(int[] array)
+{
+ Random randgenerator = new Random();
+ for (int i = 0; i < 8; i++)
+ {
+    array[i] =randgenerator.Next(0,100);
+ }
+}
+// вывод масива на экран 
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
